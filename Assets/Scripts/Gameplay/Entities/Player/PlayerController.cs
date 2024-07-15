5629ce88032443ecb8cd47b28c3b3e32c5d9f95e
Michael Sharko <michael_sharko@outlook.com>
2024-07-15 15:22:02 +0300

    Добавил перемещение персонажа по сцене
    На скорую руку было реализовано перемещение персонажа по кнопкам WASD.
    
    WS - движение вперед/взад;
    AD - поворот влево/вправо.

---
+++ Assets/Scripts/Gameplay/Entities/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(
+    typeof(CharacterController)
+)]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CharacterController _controller;
+
+    [SerializeField] private float _movementSpeed = 100.0f;
+    [SerializeField] private float _rotationSpeed = 100.0f;
+
+    private void Awake()
+    {
+        if (_controller != null) _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        float moveInput = Input.GetAxis("Vertical");
+        Vector3 moveDirection = transform.forward * moveInput * Time.deltaTime * _movementSpeed;
+        _controller.Move(moveDirection);
+    }
+
+    private void HandleRotation()
+    {
+        float rotateInput = Input.GetAxis("Horizontal");
+        Vector3 rotateDirection = Vector3.up * rotateInput * Time.deltaTime * _rotationSpeed;
+        transform.Rotate(rotateDirection);
+    }
+}
