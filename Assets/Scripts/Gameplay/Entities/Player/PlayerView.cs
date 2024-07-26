efb8ccd2eb5b053369e3ca8a58fbaf3588cad18b
Michael Sharko <michael_sharko@outlook.com>
2024-07-26 18:57:07 +0300

    Reapply "Merge branch 'Development' into Code-Main"
    This reverts commit 8e9e94cac2c4cada419d41cd12abe544d548a7c7.

--- Assets/Scripts/Gameplay/Entities/Player/PlayerView.cs
+++ Assets/Scripts/Gameplay/Entities/Player/PlayerView.cs
@@ -1,9 +1,26 @@
+using Spark.Gameplay.Weapons;
+using Spark.UI;
 using UnityEngine;
 
 namespace Spark.Gameplay.Entities.Player
 {
     public class PlayerView : MonoBehaviour
     {
+        [SerializeField] UIController _uiController;
 
+        private void Start()
+        {
+            if (_uiController == null) _uiController = GetComponent<UIController>();
+        }
+
+        public void UpdateWeaponUI(Weapon weapon)
+        {
+            _uiController.UpdatePlayerWeaponUI(weapon);
+        }
+
+        public void UpdateWeaponRangedAmmoUI(RangedWeapon rangedWeapon)
+        {
+            _uiController.UpdatePlayerRangedWeaponAmmoUI(rangedWeapon);
+        }
     }
 }
