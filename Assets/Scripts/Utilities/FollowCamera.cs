dda42da1a236206428be07777ec2f6a271f31688
SparkTeamIndieGame <spark.teame.indie.games@gmail.com>
2024-08-02 23:30:49 +0300

    Update

--- Assets/Scripts/Utilities/FollowCamera.cs
+++ Assets/Scripts/Utilities/FollowCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Spark.Utilities
@@ -7,7 +8,16 @@ namespace Spark.Utilities
         [SerializeField] Transform _player;
         [SerializeField] Vector3 _offset = new Vector3(0.0f, 15.0f, -10.0f);
 
-        void LateUpdate() => transform.position = _player.transform.position + _offset;
+        //void LateUpdate() => transform.position = _player.transform.position + _offset;
+
+        private void Update()
+        {
+            this.transform.LookAt(_player);
+            if (this.transform.position.z >= 69.0f )
+            {
+                this.transform.position = new Vector3(transform.position.x,transform.position.y , 70.0f);
+            }
+        }
 
         public void Zoom(float zooming) => _offset.y -= zooming;
     }
