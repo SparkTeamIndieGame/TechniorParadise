5c995eef1201a25f53c6f1dc7d15affd4c62b50c
SavDen <zuzinden00@gmail.com>
2024-08-03 21:14:41 +0300

    собрал Альфу версию уровня
    раскинул материалы на модели для сцены, убрал зум на камере

--- Assets/Scripts/Utilities/FollowCamera.cs
+++ Assets/Scripts/Utilities/FollowCamera.cs
@@ -7,18 +7,19 @@ namespace Spark.Utilities
     {
         [SerializeField] Transform _player;
         [SerializeField] Vector3 _offset = new Vector3(0.0f, 15.0f, -10.0f);
+        private float i = 0;
 
-        //void LateUpdate() => transform.position = _player.transform.position + _offset;
+        void LateUpdate() => transform.position = _player.transform.position + _offset;
 
-        private void Update()
-        {
-            this.transform.LookAt(_player);
-            if (this.transform.position.z >= 69.0f )
-            {
-                this.transform.position = new Vector3(transform.position.x,transform.position.y , 70.0f);
-            }
-        }
+        //private void Update()
+        //{
+        //    this.transform.LookAt(_player);
+        //    if (this.transform.position.z >= 69.0f )
+        //    {
+        //        this.transform.position = new Vector3(transform.position.x,transform.position.y , 70.0f);
+        //    }
+        //}
 
-        public void Zoom(float zooming) => _offset.y -= zooming;
+        public void Zoom(float zooming) => i -= zooming;
     }
 }
