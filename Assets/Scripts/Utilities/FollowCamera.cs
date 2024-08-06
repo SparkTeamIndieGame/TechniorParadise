8005b97336be742edd53b8ac6141b580f668779d
Michael Sharko <michael_sharko@outlook.com>
2024-08-06 20:26:02 +0300

    Микро-обновления FollowCamera
    Теперь не нужно прописывать смещение камеры по игроку. Оно вычисляется автоматически из стартовой позиции на сцене!

--- Assets/Scripts/Utilities/FollowCamera.cs
+++ Assets/Scripts/Utilities/FollowCamera.cs
@@ -6,20 +6,10 @@ namespace Spark.Utilities
     public class FollowCamera : MonoBehaviour
     {
         [SerializeField] Transform _player;
-        [SerializeField] Vector3 _offset = new Vector3(0.0f, 15.0f, -10.0f);
-        private float i = 0;
+        Vector3 _offset;
+
+        private void Start() => _offset = transform.position - _player.position;
 
         void LateUpdate() => transform.position = _player.transform.position + _offset;
-
-        //private void Update()
-        //{
-        //    this.transform.LookAt(_player);
-        //    if (this.transform.position.z >= 69.0f )
-        //    {
-        //        this.transform.position = new Vector3(transform.position.x,transform.position.y , 70.0f);
-        //    }
-        //}
-
-        public void Zoom(float zooming) => i -= zooming;
     }
 }
