48a754b9b1fc41a8404aab3ef736d0f19a9f2be3
SparkTeamIndieGame <spark.teame.indie.games@gmail.com>
2024-08-20 20:53:23 +0300

    debug

---
+++ Assets/Scripts/Comix/LoadingWindow.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LoadingWindow : MonoBehaviour
+{
+    public GameObject loadingWindow;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
