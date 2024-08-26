62b22f2f474e3c047d6561fc776d967df13517c0
SparkTeamIndieGame <spark.teame.indie.games@gmail.com>
2024-08-26 10:58:13 +0300

    Update

---
+++ Assets/newScripts/Enemies/HPbarLook.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPbarLook : MonoBehaviour
+{
+    private Camera cam;
+    private Vector3 target;
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+       target = cam.transform.position - this.transform.position;
+       transform.rotation = Quaternion.LookRotation(target);
+    }
+}
--- Assets/newScripts/Player/enemyTEST.cs
+++ Assets/newScripts/Player/enemyTEST.cs
@@ -1,11 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemyTEST : MonoBehaviour
 {
     public float MaxHP = 100;
     public float HP = 100;
+    
+    private Image img;
+
+    private void Awake()
+    {
+        img = transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        print(img.gameObject.name);
+    }
+
     public void TakeDamage(float damage)
     {
         HP -= damage;
